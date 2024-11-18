a24cfcb9713d57ab110d51123463ae3d7fb10ba5
jpferraz-git <joao.fbezerra@sptech.school>
2024-11-18 19:35:10 -0300

    Atualizações no site

--- Site/public/js/sessao.js
+++ Site/public/js/sessao.js
@@ -2,14 +2,11 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-    var cpf = sessionStorage.CPF;
 
-    var b_usuario = document.getElementById("b_usuario");
-    var b_cpf = document.getElementById("b_cpf");
+    var b_email = document.getElementById("b_email");
 
     if (email != null && nome != null) {
-        b_usuario.innerHTML = nome;
-        b_cpf.innerHTML = cpf;
+        b_email.innerHTML = nome;
 
     } else {
         window.location = "../login.html";
---
+++ gymimpact.sql
@@ -0,0 +1,19 @@
+create database gymimpact;
+
+use gymimpact;
+
+create table Usuario(
+idUsuario int primary key auto_increment,
+nome varchar(100),
+email varchar (100),
+senha varchar(100)
+);
+
+
+
+select * from usuario;
+
+use aquatech;
+
+select * from usuario;
+
